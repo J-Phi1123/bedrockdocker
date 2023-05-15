25cb8585cb2f6362296beebc18c70bacfe412f63
jack <goosegetsit@gmail.com>
2023-05-15 08:58:39 -0700

    Auto-build-Mon May 15 08:58:39 PDT 2023

--- build_it.sh
+++ build_it.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Script to build, push, and commit image
-
+docker logout
+docker login -u jackclark1123 -p d9bfa568-8669-4481-be26-aaf90074760d
 docker build -t jackclark1123/bedrockserver .
 docker push jackclark1123/bedrockserver
 git add .
