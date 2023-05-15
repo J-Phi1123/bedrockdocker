6fc140316b5d597f74f89db718f0f55ff476e116
jack <goosegetsit@gmail.com>
2023-05-15 08:59:24 -0700

    Auto-build-Mon May 15 08:59:24 PDT 2023

--- build_it.sh
+++ build_it.sh
@@ -5,6 +5,7 @@ docker logout
 docker login -u jackclark1123 -p d9bfa568-8669-4481-be26-aaf90074760d
 docker build -t jackclark1123/bedrockserver .
 docker push jackclark1123/bedrockserver
+docker logout
 git add .
 git commit -m "Auto-build-`date`"
 git push
