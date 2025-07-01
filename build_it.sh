0a3bdb793ebeb5cc217ad2a5095ba14a663d8380
jack <goosegetsit@gmail.com>
2025-07-01 11:18:03 -0700

    Moved password out and add auto download

--- build_it.sh
+++ build_it.sh
@@ -2,7 +2,7 @@
 
 # Script to build, push, and commit image
 docker logout
-docker login -u jackclark1123 -p d9bfa568-8669-4481-be26-aaf90074760d
+docker login -u jackclark1123 -p `cat pass`
 docker build -t jackclark1123/bedrockserver .
 docker push jackclark1123/bedrockserver
 docker logout
